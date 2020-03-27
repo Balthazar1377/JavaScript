d9e97faf1f14fa6cb4984a04bf2b1ce0c487554d
Sergey Uglik <antihate1377@gmail.com>
2020-03-27 12:12:33 +0300

    Сделал второй урок(обязательное задание)

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-let money,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
-    
-alert('This is the first lesson');
-console.log('Some words in console...');
+let money = 500,
+    income = 300,
+    addExpenses = '200$ на девушку, 100$ на себя, 50$ на книги',
+    deposit = true,
+    mission = 1500,
+    period = 1;
+
+console.log(typeof money );
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель: заработать ' + mission + ' долларов');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+
+let budgetDay = money / 30;
+console.log(budgetDay);
