b671e64cc4777d51fa84c119750b40f0f589a552
Sergey Uglik <antihate1377@gmail.com>
2020-03-30 22:43:04 +0300

    feat: complete lesson 3

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+'use strict';
 let money = 500,
     income = 300,
     addExpenses = '200$ на девушку, 100$ на себя, 50$ на книги',
@@ -16,3 +17,34 @@ console.log(addExpenses.split(', '));
 
 let budgetDay = money / 30;
 console.log(budgetDay);
+
+// The thirst lesson
+
+money = prompt('Ваш месячный доход?');
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+deposit = confirm(Boolean('Есть ли у вас депозит в банке?'));
+console.log(deposit);
+
+ let expenses01 = prompt('Введите обязательную статью расходов'),
+amount01 = +prompt('Во сколько это обойдется?'),
+expenses02 = prompt('Введите обязательную статью расходов'),
+amount02 = +prompt(' Во сколько это обойдется?'),
+budgetMonth = money - (amount01 + amount02);
+console.log(budgetMonth);
+
+ let missionComplete = Math.ceil(mission / budgetMonth);
+console.log(missionComplete);
+
+let budgetDayStrict = Math.floor(budgetMonth / 30);
+console.log(budgetDayStrict);
+
+if (budgetDay >= 1200){
+    alert('У вас высокий уровень дохода');
+} else if (1200 > budgetDay >=600){
+    alert('У вас средний уровень дохода');
+} else if (budgetDay < 600){
+    console.log('К сожалению, ваш уровень дохода ниже среднего');
+} else if (budgetDay < 0){
+    //Не было сказано, выводить в консоль или на экран
+    alert('Что-то пошло не так');    
+}
