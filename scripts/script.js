9cdef6490bfe12839cdd619b49437b6149e12327
Sergey Uglik <antihate1377@gmail.com>
2020-05-10 22:27:24 +0300

    feat: finish lesson25

--- scripts/script.js
+++ scripts/script.js
@@ -327,5 +327,76 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     };
     calc(100);
+
+    //send-ajax-form
+    const sendForm = () => {
+        const errorMessage = 'Что-то пошло не так!',
+            loadMessage = 'Загрузка...',
+            successMessage = 'Спасибо! Мы скоро свяжемся с Вами!';
+        const form = document.querySelectorAll('form'),
+            message = document.querySelector('.mess');
+        const statusMessage = document.createElement('div');
+        statusMessage.style.cssText = 'font-size: 2rem;';
+        const postData = (body, outputData, errorData) => {
+            const request = new XMLHttpRequest();
+            request.addEventListener('readystatechange', () => {
+                if (request.readyState !== 4) {
+                    return;
+                }
+                if (request.status === 200) {
+                    outputData();
+
+                } else {
+                    errorData(request.status);
+                }
+            });
+            request.open('POST', './server.php');
+            request.setRequestHeader('Content-Type', 'application/json');
+            request.send(JSON.stringify(body));
+        };
+
+        form.forEach(elem => {
+            const tel = elem.querySelector('input[type="tel"]');
+            tel.addEventListener('input', () => {
+                tel.value = tel.value.replace(/[^+\d]/g, '');
+            });
+            const text = elem.querySelector('input[type="text"]');
+            text.addEventListener('input', () => {
+                text.value = text.value.replace(/[^а-я\s]/ig, '');
+            });
+            message.addEventListener('input', () => {
+                message.value = message.value.replace(/[^а-я\s]/ig, '');
+            });
+            // message.addEventListener('input', () => {
+            //     message.value = message.value.replace(/[^а-я\s]/ig, '');
+            // });
+            elem.addEventListener('submit', event  => {
+                event.preventDefault();
+                elem.appendChild(statusMessage);
+                statusMessage.textContent = loadMessage;
+                const formData = new FormData(elem);
+                const body = {};
+                formData.forEach((val, key) => {
+                    body[key] = val;
+                });
+                const inputs = elem.querySelectorAll('input');
+                postData(body,
+                    () => {
+                        statusMessage.textContent = successMessage;
+                        inputs.forEach(item => {
+                            item.value = '';
+                        });
+                    },
+                    error => {
+                        statusMessage.textContent = errorMessage;
+                        inputs.forEach(item => {
+                            item.value = '';
+                        });
+                    }
+                );
+            });
+        });
+    };
+    sendForm();
 });
 
